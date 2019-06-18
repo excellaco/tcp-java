3f37462875d2e2b598925f999080dac7171a3281
James M. Stricker <jmstricker93@gmail.com>
2019-06-18 13:44:14 -0400

    initial setup attempt

---
+++ src/integration-test/java/com/excella/reactor/controllers/ControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.excella.reactor.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class ControllerIntegrationTest {
+
+    @Test
+    private void pass(){
+        assert(true);
+    }
+}
