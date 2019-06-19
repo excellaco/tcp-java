b5ce71bfa3eb66e0e508a559c775b37d4dff5359
Kenneth Russell <kenrusselldc@gmail.com>
2019-06-19 12:38:01 -0400

    context fix

--- src/integration-test/java/com/excella/reactor/controllers/ControllerSmokeTest.java
+++ src/integration-test/java/com/excella/reactor/controllers/ControllerSmokeTest.java
@@ -3,6 +3,7 @@ package com.excella.reactor.controllers;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
