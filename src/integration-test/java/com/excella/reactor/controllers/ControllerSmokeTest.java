a5d1e94d890c204e638d7d2e057f71d2ebe0d2f5
James M. Stricker <jmstricker93@gmail.com>
2019-06-19 12:18:21 -0400

    unsuccessful attempt to get int tests running properly

--- src/integration-test/java/com/excella/reactor/controllers/ControllerSmokeTest.java
+++ src/integration-test/java/com/excella/reactor/controllers/ControllerSmokeTest.java
@@ -1,24 +1,20 @@
 package com.excella.reactor.controllers;
 
-import com.excella.reactor.shared.SampleEntity;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@ComponentScan("com.excella.reactor")
 @SpringBootTest
-public class ControllerSmokeTest {
-
-  @Autowired private CrudController<SampleEntity> controller;
-
+public class ControllerSmokeTest extends AbstractTestNGSpringContextTests {
+  @Autowired private EmployeeController controller;
   @Test
-  public void contexLoads() throws Exception {
+  public void contextLoads() throws Exception {
     assertThat(controller).isNotNull();
   }
 }
