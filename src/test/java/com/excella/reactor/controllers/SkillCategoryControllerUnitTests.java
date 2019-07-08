7f562290c042b0a7ed94a8324ebb7d0e5d6ad6f5
Walter <walterc@vt.edu>
2019-07-08 13:13:46 -0400

    update controller tests to only test getService method

--- src/test/java/com/excella/reactor/controllers/SkillCategoryControllerUnitTests.java
+++ src/test/java/com/excella/reactor/controllers/SkillCategoryControllerUnitTests.java
@@ -1,50 +1,27 @@
 package com.excella.reactor.controllers;
 
-import com.excella.reactor.domain.SkillCategory;
 import com.excella.reactor.service.SkillCategoryService;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
-import java.util.Arrays;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class SkillCategoryControllerUnitTests {
 
-    private SkillCategoryService mockService;
-    private SkillCategory cat1;
-    private SkillCategory cat2;
-    private SkillCategory cat3;
+
+    private SkillCategoryService skillCategoryService;
     private SkillCategoryController skillCategoryController;
+
 
     @BeforeMethod
     private void beforeEach() {
-        mockService = mock(SkillCategoryService.class);
-        cat1 = new SkillCategory();
-        cat2 = new SkillCategory();
-        cat3 = new SkillCategory();
-
-        skillCategoryController = new SkillCategoryController(mockService);
-
+        skillCategoryService = mock(SkillCategoryService.class);
+        skillCategoryController = new SkillCategoryController(skillCategoryService);
     }
 
     @Test
-    public void getAll_can_return_flux_of_multiple_skill_categories() {
-        when(mockService.all(null)).thenReturn(Flux.just(cat1, cat2, cat3));
-
-        StepVerifier.create(skillCategoryController.getAll(null))
-                .expectNextSequence(Arrays.asList(cat1, cat2, cat3))
-                .expectComplete()
-                .verify();
-    }
-
-    @Test
-    public void getAll_can_return_flux_of_no_skill_categories() {
-        when(mockService.all(null)).thenReturn(Flux.empty());
-
-        StepVerifier.create(skillCategoryController.getAll(null)).expectComplete().verify();
+    public void testGetService() {
+        Assert.assertEquals(skillCategoryService, skillCategoryController.getService());
     }
 }
--- src/test/java/com/excella/reactor/controllers/SkillControllerUnitTests.java
+++ src/test/java/com/excella/reactor/controllers/SkillControllerUnitTests.java
@@ -1,50 +1,25 @@
 package com.excella.reactor.controllers;
 
-import com.excella.reactor.domain.Skill;
 import com.excella.reactor.service.SkillService;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
-import java.util.Arrays;
-
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class SkillControllerUnitTests {
 
-    private SkillService mockService;
-    private Skill skill1;
-    private Skill skill2;
-    private Skill skill3;
+    private SkillService skillService;
     private SkillController skillController;
+
 
     @BeforeMethod
     private void beforeEach() {
-        mockService = mock(SkillService.class);
-        skill1 = new Skill();
-        skill2 = new Skill();
-        skill3 = new Skill();
-
-        skillController = new SkillController(mockService);
-
+        skillService = mock(SkillService.class);
+        skillController = new SkillController(skillService);
     }
 
     @Test
-    public void getAll_can_return_flux_of_multiple_skills() {
-        when(mockService.all(null)).thenReturn(Flux.just(skill1, skill2, skill3));
-
-        StepVerifier.create(skillController.getAll(null))
-                .expectNextSequence(Arrays.asList(skill1, skill2, skill3))
-                .expectComplete()
-                .verify();
-    }
-
-    @Test
-    public void getAll_can_return_flux_of_no_skills() {
-        when(mockService.all(null)).thenReturn(Flux.empty());
-
-        StepVerifier.create(skillController.getAll(null)).expectComplete().verify();
+    public void testGetService() {
+        Assert.assertEquals(skillService, skillController.getService());
     }
 }
